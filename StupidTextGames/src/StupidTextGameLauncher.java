7d513248ff35fd4fdffd88ae213b69a2a31184aa
Stonedahl <forreststonedahl@augustana.edu>
2018-08-22 12:34:56 -0500

    project works with two stupid games, but the code is ugly

--- StupidTextGames/src/StupidTextGameLauncher.java
+++ StupidTextGames/src/StupidTextGameLauncher.java
@@ -1,8 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class StupidTextGameLauncher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Choose a stupid text game:");
+		System.out.println("1: UnluckyDice");
+		System.out.println("2: ChickenToss");
+		
+		Scanner console = new Scanner(System.in);
+		int choice = console.nextInt();
+		
+		if (choice == 1) {
+			Random randGen = new Random();		
+			int roll = randGen.nextInt(6) + 1;
+			System.out.println("You rolled " + roll);
+			if (roll < 3) {
+				System.out.println("Too small, you lose!");
+			} else {
+				System.out.println("Too big, you lose!");
+			}
+		} else if (choice == 2) {
+			System.out.println("You toss the chicken, and it lands on your head.");
+			System.out.println("You lose.");
+		}		
 
 	}
 
