15f96bc7f1f577c455fdc788492aaf4650978e36
Yafet <yafetzeleke17@augustana.edu>
2018-08-27 02:55:17 -0500

    Added Eclipse project to repo.

---
+++ StupidTextGames/src/CheeseThrowGame.java
@@ -0,0 +1,15 @@
+//Name: Yafet Zeleke
+import java.util.Scanner;
+
+	public class CheeseThrowGame implements StupidTextGame {
+		
+		public String getName() {
+			return "Cheese Throw";
+		}
+		
+		public void play(Scanner console) {
+			System.out.println("You throw the cheese and try to get it in the ring, and you miss.");
+			System.out.println("You miss.");
+		}
+
+	}
